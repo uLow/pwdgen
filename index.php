539dc95383367dea1fe4849f1a6217a058511a74
Vitalijs Litvinovs <dev@codin.pro>
2017-03-29 15:46:24 +0300

    notice fix

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if ($_POST) {
     $types = isset($_POST['types']) ? array_keys($_POST['types']) : [];
 
     $view->setVar('length', $length);
-    $view->setVar('types', $_POST['types']);
+    $view->setVar('types', isset($_POST['types']) ?: []);
 
     try {
         $generator = new \CodinPro\Lib\PasswordGenerator($length, $types);
